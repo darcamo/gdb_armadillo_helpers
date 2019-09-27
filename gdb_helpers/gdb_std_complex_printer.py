b541af719ca08c3ac4321998b3e8c3e0d1270235
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-09-27 11:14:15 -0300

    Show abs and phase of complex numbers in std::complex pretty printer

--- gdb_helpers/gdb_std_complex_printer.py
+++ gdb_helpers/gdb_std_complex_printer.py
@@ -1,3 +1,5 @@
+import cmath
+import math
 
 
 class StdComplexIntPrinter:
@@ -22,11 +24,12 @@ class StdComplexDoublePrinter:
         self.val = val.cast(gdb.lookup_type("double").array(1))
         self.real_part = self.val[0]
         self.imag_part = self.val[1]
+        self.c = complex(self.real_part, self.imag_part)
 
     def to_string(self):
-        if self.imag_part >= 0:
-            return "{0} + {1}i".format(self.real_part, self.imag_part)
-        return "{0} - {1}i".format(self.real_part, abs(self.imag_part))
+        angle = 180.0 * cmath.phase(self.c) / math.pi
+        sign = "+" if self.imag_part >= 0 else "-"
+        return "{0:.4} {1} {2:.4}i ({3:.2}:{4:.2}°)".format(float(self.real_part), sign, abs(float(self.imag_part)), abs(self.c), angle)
 
 
 
